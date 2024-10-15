532deee7b658b47a4be2b9fee436aaf34e697de1
João Taranto <tarantoj04@gmail.com>
2024-10-15 10:11:09 -0300

    feat: connect to mongoDB database

---
+++ index.js
@@ -0,0 +1,17 @@
+// FRAMEWORKS IMPORT
+const express = require("express");
+const dotenv = require("dotenv");
+
+// ARCHIVES IMPORT
+const connectDatabase = require("./src/database/mongoose.database");
+
+dotenv.config();
+
+const app = express(); // Cria uma aplicação Express
+connectDatabase();
+app.get("/tasks", (req, res) => {
+    const tasks = [{ description: "Estudar programação", isCompleted: false }];
+    res.status(200).send(tasks);
+});
+
+app.listen(8000, () => console.log("Listening on port 8000")); // Iniciar aplicação Express na porta 8000
---
+++ src/database/mongoose.database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.uwcnc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+        );
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Failed to connect MongoDB!");
+    }
+};
+
+module.exports = connectDatabase;
