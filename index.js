b25153bb47304e0ad8ab51a5c3c94b5455a599b1
JoaoTaranto <tarantoj04@gmail.com>
2025-02-04 11:24:08 -0300

    feat: "add router file to task entity"

--- index.js
+++ index.js
@@ -1,90 +1,20 @@
 // FRAMEWORKS IMPORT
 const express = require("express");
 const dotenv = require("dotenv");
+const TaskRouter = require("./src/routes/task.routes")
 
 // ARCHIVES IMPORT
 const connectDatabase = require("./src/database/mongoose.database");
-const TaskModel = require("./src/models/task.model");
+
 
 dotenv.config();
 
 const app = express(); // Cria uma aplicação Express
 app.use(express.json()); // Informa ao app Express que receberá JSON nas requests (req_body)
+
 connectDatabase();
 
-// GET ALL TASKS
-app.get("/tasks", async (req, res) => {
-    try {
-        const tasks = await TaskModel.find({});
-        res.status(200).send(tasks);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+app.use("/tasks", TaskRouter)
 
-// GET TASKS BY ID
-
-app.get("/tasks/:id", async (req, res) => {
-    const taskId = req.params.id;
-
-    const task = await TaskModel.findById(taskId);
-    if (!task) {
-        res.status(404).send("Esta tarefa não foi encontrada!");
-    }
-    res.status(200).send(task);
-});
-
-// CREATE TASK
-app.post("/tasks", async (req, res) => {
-    try {
-        const newTask = new TaskModel(req.body);
-        await newTask.save();
-        res.status(201).send(newTask);
-    } catch (error) {
-        console.log(error.message);
-    }
-});
-
-// DELETE TASK
-app.delete("/tasks/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskToDelete = await TaskModel.findById(taskId);
-
-        if (!taskToDelete) {
-            return res.status(404).send("Esta tarefa não foi encontrada!");
-        }
-        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
-        res.status(200).send(deletedTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.patch("/tasks/:id", async (req, res) => {
-    try {
-        
-        const taskId = req.params.id;
-        const taskData = res.body 
-
-        const taskToUpdate = await TaskModel.findById(taskId);
-        const allowedUpdates = ["isCompleted"];
-        const requestedUpdates = Object.keys(taskData)
-
-        for (update of requestedUpdates) {
-            if (allowedUpdates.includes(update)) {
-                taskToUpdate[update] = taskData[update]
-            } else {
-                res.status(500).send("Um ou mais campos inseridos não são editáveis!")
-            }
-        }
-        await taskToUpdate.save()
-        return res.status(200).send(taskToUpdate)
-
-    }
-    catch (error) {
-        return res.status(500).send(error.message);
-    }
-})
 
 app.listen(8000, () => console.log("Listening on port 8000")); // Iniciar aplicação Express na porta 8000
